5dac53174aacb16b40ba6fb58dc6532982940d08
Pavani T <pavanit.0810@gmail.com>
2025-02-08 16:50:06 +0530

    Implementing VM creation in Azure using Terraform

--- 03-resource/data.tf
+++ 03-resource/data.tf
@@ -3,6 +3,6 @@ data "azurerm_resource_group" "example" {
 }
 data "azurerm_subnet" "example" {
   name                 = "default"
-  virtual_network_name = "project-setup-network"
+  virtual_network_name = "project-network"
   resource_group_name  = data.azurerm_resource_group.example.name
 }
