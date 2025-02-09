b1983256227cc939bc3bcbfd09fef9c025304780
Pavani T <pavanit.0810@gmail.com>
2025-02-09 10:38:25 +0530

    Implementing loops-count in Terraform

--- 07-loops-count/main.tf
+++ 07-loops-count/main.tf
@@ -1,3 +1,12 @@
 resource "null_resource" "demo" {
-  count = 10
+  count = length(var.demo)
+  provisioner "local-exec" {
+    command = "Fruit ${var.demo[count.index]}"
+  }
+}
+variable "demo" {
+  default = [
+              "apple",
+              "banana"
+  ]
 }
