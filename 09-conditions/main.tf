9f6d67915f835cfeaf586318f941ed13cc4bc24f
Pavani T <pavanit.0810@gmail.com>
2025-02-10 11:45:14 +0530

    Implementing Conditions in Terraform

--- 09-conditions/main.tf
+++ 09-conditions/main.tf
@@ -1,3 +1,11 @@
+terraform {
+  required_providers {
+    null = {
+      source  = "hashicorp/null"
+      version = "3.2.2"
+    }
+  }
+}
 variable "a" {}
 
 output "test" {
@@ -5,4 +13,10 @@ output "test" {
 }
 resource "null_resource" "test1" {
   count = var.a > 10 ? 1 : 0
+}
+locals {
+  count = var.a > 10 ? 1 : 0
+}
+resource "null_resource" "test2" {
+  count = local.count
 }
