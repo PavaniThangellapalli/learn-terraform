a8d4ef8c69b184ed809f45c9a17500156d0c3221
Pavani T <pavanit.0810@gmail.com>
2025-02-15 02:01:08 +0530

    Retrieving Vault secrets using Terraform

---
+++ 11-vault/main.tf
@@ -0,0 +1,13 @@
+provider "vault" {
+  address = "http://vault-public.pavanidevops.online:8200"
+  token = var.token
+}
+variable "token" {}
+# search for data source “vault_generic_secret” in terraform doc, for retrieving the password
+data "vault_generic_secret" "secret_data" {
+  path = "test/demo-ssh"
+}
+resource "local_file" "local" {
+  filename = "/tmp/pass"
+  content = data.vault_generic_secret.secret_data.data["password"]
+}
